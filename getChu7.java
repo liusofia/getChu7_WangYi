c2003f6aeb381ffcb1553c883842db4c6e0bb8a9
liusofia <42337552+liusofia@users.noreply.github.com>
2018-08-13 16:14:49 +0800

    first commit for inital java file in 2018.08.13

---
+++ getChu7.java
@@ -0,0 +1,14 @@
+class getChu7{
+	public static void main(String[] args){
+    		getChu7(100);
+	}
+
+	public static void getChu7(int a){
+   		for(int i = 0;i <= a;i++){
+       			if(i % 7 == 0){
+        		System.out.println(i);
+    			}
+		}
+	}
+}
+
